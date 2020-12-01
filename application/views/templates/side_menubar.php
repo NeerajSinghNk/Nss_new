14bec5b371721d50fa2cd4f7133fb3ecd5a32b49
NeerajSinghNk <neeraj09kumar09@gmail.com>
2020-12-01 14:25:23 +0530

    side menu

--- application/views/templates/side_menubar.php
+++ application/views/templates/side_menubar.php
@@ -13,7 +13,14 @@
 
         
         <li id="manageUserSubNav"><a href="<?php echo base_url('Dashboard/list')?>" ><i class="ion ion-android-people"></i><span>Overall Responses</span></a></li>
+        <li id="createUserSubNav"><a href="<?php echo base_url('Dashboard/currentList')?>" ><i class="ion ion-android-people"></i><span>Current Responses</span></a></li>
+        <li id="createUserSubNav"><a href="<?php echo base_url('Dashboard/showReview')?>" ><i class="ion ion-android-people"></i><span>Under Review Application</span></a></li>
+        <li id="createUserSubNav"><a href="<?php echo base_url('Dashboard/summary')?>" ><i class="fa fa-pie-chart"></i><span>Graphical Summary</span></a></li>
+        
+       
         <li id="createUserSubNav"><a href="<?php echo base_url('Dashboard/nssRegisUpdate')?>" ><i class="ion ion-android-calendar"></i><span>Create New Registration Date</span></a></li>
+        
+        
         <!-- <li id="manageUserSubNav"><a href="<?php echo base_url('Dashboard/update') ?>" ><i class="fa fa-circle-o"></i><span>Update Registration</span></a></li> -->
         <li><a href="<?php echo base_url('Admin/logout') ?>"><i class="glyphicon glyphicon-log-out"></i> <span>Logout</span></a></li>
         
